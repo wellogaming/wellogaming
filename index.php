ebadcf0da79d61fc6ddb4af4aa268f6b78d0cc5d
wellogaming <75124238+wellogaming@users.noreply.github.com>
2020-11-27 05:29:54 -0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Home: WelloGaming</title>
+	<link rel="stylesheet" type="text/css" href="css/style.css">
+	<link rel="stylesheet" type="text/css" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+</head>
+<body>
+<div class="container">
+	<div class="menu">
+		<ul>
+			<li class="active">Home</li>
+			<a href="downloads.php" class="w"><li>Downloads</li></a>
+			<a href="contact.php" class="w"><li>Contact</li></a>
+			<a href="about.php" class="w"><li>About Us</li></a>
+			<li><a href="signup.php" class="signup-btn"><span>Sign Up</span></a></li>
+		</ul>
+	</div>
+	<div class="banner">
+		<div class="app-text">
+			<h1>WelloGaming. <br> The gamers gang.</h1>
+			<p>WelloGamers are the best people in the world.</p>
+			<div class="play-btn">
+				<div class="play-btn-inner">
+					<a href="about-depth.php" class="d"><i class="fa fa-play"></i></a>
+				</div>
+				<small><b>LEARN MORE</b></small>
+			</div>
+		</div>
+	</div>
+	<div class="quick-links">
+		<ul>
+			<li><p>GAMES</p></li>
+			<li><p>RECENT NEWS</p></li>
+			<li><p>SCRIPTS</p></li>
+		</ul>
+	</div>
+	<div class="social-icons">
+		<ul>
+			<li><a href="https://twitter.com/tahtnewbie"><i class="fa fa-twitter"></i></a></li>
+		</ul>
+	</div>
+</div>
+</body>
+</html>
